d8420a1dd1a0b5d8376ab6fe399f2341b42ed901
Vaibhav Kumar <vaibhaviiit@gmail.com>
2020-04-08 23:09:25 +0530

    Update promise-all-pollyfill.js
    Implemented pollyfill for promise all

--- promise-all-pollyfill.js
+++ promise-all-pollyfill.js
@@ -1,1 +1,26 @@
-
+// PollyFill for promiseAll
+function promiseAll(promises) {
+  return new Promise(function(resolve, reject) {
+    let promiseCount = promises.length;
+    let resolveData = [];
+    let resolveCount = 0;
+    
+    function checkStatus(data) {
+      resolveData.push(data);
+      resolveCount++;
+      if (resolveCount == promiseCount) {
+        resolve(resolveData);
+      }
+    }
+    
+    promises.forEach(function(promise, i) {
+      promise.then(
+        function(data) {
+          checkStatus(data);
+        }           
+      ).catch((error) => {
+        reject(error);
+      });
+    });
+  });
+}
