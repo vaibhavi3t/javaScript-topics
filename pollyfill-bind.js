44b695c867b0b999cca752807a7e57af1205d767
Vaibhav Kumar <vaibhaviiit@gmail.com>
2020-04-08 16:49:07 +0530

    Update pollyfill-bind.js
    Implemented pollyfill for bind

--- pollyfill-bind.js
+++ pollyfill-bind.js
@@ -1,1 +1,13 @@
-
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(context) {
+    let func = this;
+    var boundArguments = Array.prototype.slice.call(arguments, 1);
+    return function() {
+      var targetArguments = Array.prototype.slice.call(arguments);
+      return func.apply(
+        context,
+        boundArguments.concat(targetArguments)
+      );
+    }
+  }
+}
