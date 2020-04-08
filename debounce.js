5bf8b3837025cd5eb41fe40fc26ccb057a8a005b
Vaibhav Kumar <vaibhaviiit@gmail.com>
2020-04-08 22:59:28 +0530

    Update debounce.js
    Implemented debiunce

--- debounce.js
+++ debounce.js
@@ -1,1 +1,12 @@
-
+function debounce(wait, func) {
+  var timeout = null;
+  
+  return function(...args) {
+    let context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(
+      func.apply(this, ...args),
+      wait
+    )
+  }
+}
